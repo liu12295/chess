f578769501c6eeb0064b313446bb235a193f6f41
Jack Liu <liu.jiaji@gmail.com>
2016-05-15 16:29:30 -0700

    Add copyright header.

--- chess.py
+++ chess.py
@@ -1,5 +1,25 @@
+# The MIT License (MIT)
 #
+# Copyright (c) 2016, Jack Liu
+# All rights reserved.
 #
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
 from bs4 import BeautifulSoup
 import requests
@@ -28,8 +48,11 @@ class Event(object):
         self.URL = URL
         self.Score = Score
 
-    def dump(self) :
+    def dump(self):
         print self.Date, self.Event, self.URL, self.Score
+
+    def __str__(self):
+        return dump(self)
 
 #
 # Player class
